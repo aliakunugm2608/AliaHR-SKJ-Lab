1e126acdecfe0743a1b2f49b9f08f082614afdb0
aliakunugm2608 <aliahafidarahmah2005@mail.ugm.ac.id>
2024-09-03 15:06:38 +0700

    Add files via upload

---
+++ Assignment 1/add_numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1= 30;
+    int num2= 10;
+    int sum;
+
+
+    // Add the numbers
+    sum = num1 + num2;
+
+    // Display the sum
+    cout << sum << endl;
+
+    return 0;
+}
---
+++ Assignment 1/mul_numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1= 5;
+    int num2= 10;
+    int result= 0;
+
+
+    // Multiply the numbers
+    result = num1*num2;
+
+    // Display the 
+    cout << result << endl;
+
+    return 0;
+}
